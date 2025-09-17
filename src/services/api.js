1f37dc9642ce77088fd5044943950171058fbfcf
tillenotuoma <totuoma27@gmail.com>
2025-09-17 16:07:55 +0300

    removed subject header from api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,8 +6,7 @@ const API_BASE_URL = 'https://capital-customer-care.onrender.com/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
-    subject: 'default',
+    'Content-Type': 'application/json'
   },
 });
 
