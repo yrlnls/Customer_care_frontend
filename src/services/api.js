5990b297e7638b82706c55d7fe38fcd37cfb740c
tillenotuoma <totuoma27@gmail.com>
2025-10-20 14:24:42 +0300

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://capital-customer-care.onrender.com/api';
+// const API_BASE_URL = 'https://capital-customer-care.onrender.com/api';
+const API_BASE_URL = 'https://customercare.capitalfibersolution.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
